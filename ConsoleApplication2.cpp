aff85aeafc80e19b03e1d3a8645b6d635318715c
ghkimwoo <45334270+ghkimwoo@users.noreply.github.com>
2022-08-31 15:08:50 +0900

    초기 커밋.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+class CUser {
+public:
+    CUser()
+    {
+        cout << "생성자 호출" << endl;
+    }
+    ~CUser() {
+        cout << "소멸자 호출" << endl;
+    }
+    int nData = 10;
+    void PrintData() {
+        cout << nData << endl;
+    }
+};
+
+int main()
+{
+    CUser user;
+    user.PrintData();
+}
